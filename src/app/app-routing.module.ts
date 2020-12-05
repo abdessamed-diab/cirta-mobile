9559c453f6024a5ad0f7d576f0dbe727809cbafb
abdessamed-diab <abdou_gl@live.fr>
2020-12-05 16:23:58 +0100

    feat: add home lazy loaded module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
