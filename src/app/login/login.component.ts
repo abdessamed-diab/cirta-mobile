372c74dc0521766f87b6c0c9baa41e1d00e10f94
abdessamed-diab <abdou_gl@live.fr>
2020-12-05 19:27:20 +0100

    fix: redirect absolute url

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {LoginService} from './login.service';
 import {FormControl} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +13,14 @@ export class LoginComponent implements OnInit {
   pseudoName = new FormControl('');
   password   = new FormControl('');
 
-  constructor(public loginService: LoginService) { }
+  constructor(public loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
+    this.pseudoName.disable({onlySelf: true, emitEvent: false});
+    this.password.disable({onlySelf: true, emitEvent: false});
+    if (this.router.url.startsWith('/home')) {
+      this.router.navigateByUrl('/home');
+    }
   }
 
   onFacebookSignIn(form: HTMLFormElement): void {
